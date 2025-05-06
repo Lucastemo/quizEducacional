16ad091fd0286abfb2adda82fce8c5b330051153
aluno <etecae023@hotmail.com>
2025-05-06 19:43:04 -0300

    Alteração no usuarioModel e no usuarioController para inserção da funcionalidade cadastrarUsuario

--- models/usuarioModel.js
+++ models/usuarioModel.js
@@ -1,11 +1,10 @@
-const db = require('../config/db');
+const db = require('../config/db.js');
 
 const usuarioModel = {
     verificarUsuario: async (email) => {
         //Método que verifica o email informado através da 'procedure'
         try {
             const [rows] = await db.execute('CALL buscar_usuario_por_email(?)', [email]);
-
             if (rows.length === 0) {
                 return res.status(404).json({error: 'Usuário não encontrado.'});
             }
@@ -14,7 +13,26 @@ const usuarioModel = {
         } catch (error) {
             return res.status(500).json({ error: 'Erro interno ao buscar usuário.'});
         }
+    },
+
+    cadastrarUsuario: async (nome, email, senha) => {  // Inserção dos campos
+        try {
+            const sql = 'CALL criar_usuario (?, ?, ?)'; // Chamada da procedure
+            
+            const [result] = await db.query(sql, [nome, email, senha]);  // Armazenamento dos campos
+            
+            if (result && result.insertId) { // Verificação caso tenha gerado ou não o ID
+                return {id: result.insertId, nome, email};
+            
+            } else {
+                throw new Error('Erro ao cadastrar usuário: ID não retornado.'); // Erro para caso não haja ID
+            }  
+        } catch (error) {
+            console.error('Erro ao cadastrar usuário', error);  // catch com erro em algum processo diferente
+            throw error;  
+        }
     }
+    
 };
 
 module.exports = usuarioModel;
