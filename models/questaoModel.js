b9438ba487058114dacf6a34353f96c07ba7e10d
Francisco <marabas.sp@gmail.com>
2025-05-09 17:37:46 -0300

    terminado questaoModel.js para conferencia do PO

--- models/questaoModel.js
+++ models/questaoModel.js
@@ -1,20 +1,63 @@
-const db = require ('../config/db');
+const db = require('../config/db');
 
-const questao ={
-
-    createQuestao: async (enuciado, dificuldade, id_disciplina) => {
-
-        try {
-            const [result] = await db.execute('CALL, criar_questao (?,?,?)',
-            [enuciado, dificuldade, id_disciplina]);
-            return {id: result.insertId, enuciado, dificuldade, id_disciplina};
-            
-        } catch (error) {
-            throw(error);
-        }
-    },
-
-    updateQustao: async () =>{
+const Questao = {
+  // Criar nova questão
+  createQuestao: async (enunciado, dificuldade, id_disciplina) => {
+    try {
+      const [rows] = await db.query('CALL inserir_questao(?, ?, ?)',
+        [enunciado, dificuldade, id_disciplina]);
         
+      return { enunciado, dificuldade, id_disciplina };
+    } catch (error) {
+      throw error;
     }
-}
+  },
+
+  // Atualizar questão
+  updateQuestao: async (id, enunciado, dificuldade) => {
+    try {
+      if (!id) {
+        throw new Error('ID da questão é obrigatório!');
+      }
+
+      await db.query('CALL editar_questao(?, ?, ?)', [id, enunciado, dificuldade]);
+
+      return { id, enunciado, dificuldade };
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  // Excluir questão
+  deleteQuestao: async (id) => {
+    try {
+      if (!id){ 
+        throw new Error('ID da questão é obrigatório!');
+    }
+
+      await db.query('CALL excluir_questao(?)', [id]);
+      return { id };
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  
+  buscarDisciplina: async (id_disciplina) => {
+    try {
+      if (!id_disciplina) {
+        throw new Error('ID_disciplina é obrigatório!');
+      }
+
+      const [rows] = await db.query('CALL buscar_questoes_disciplina(?)',
+        [id_disciplina]);
+
+      return rows[0]; 
+
+    } catch (error) {
+      throw error;
+    }
+  }
+};
+
+module.exports = Questao;
