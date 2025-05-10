69e9c1301bbdbb116af6e95bb0be87c559ab2e5d
aluno <etecae023@hotmail.com>
2025-05-09 21:20:19 -0300

    Corrigindo erros de digitação e lógica

--- app.js
+++ app.js
@@ -12,6 +12,16 @@ app.use(express.json());
 //app.get('/api/usuario', usuarioController.getAllUser);
 //app.post('/api/usuario', usuarioController.createNewUser);
 
+// Rotas Usuario
+app.post('/api/usuario', usuarioController.cadastrarUsuario);
+
+// Rotas Curso
+// app.post('/api/curso', cursoController.novoRegistro);
+
+// app.put('/api/curso/:id', cursoController.edicaoRegistro);
+
+// app.delete('/api/curso/:id', cursoController.excluirRegistro);
+
 app.listen(PORT, () => {
  console.log(`Servidor na porta http://localhost:${PORT}`);
 });
--- models/usuarioModel.js
+++ models/usuarioModel.js
@@ -6,31 +6,25 @@ const usuarioModel =  {
     cadastrarUsuario: async (nome, email, senha) => { 
         try {
             const sql = 'CALL criar_usuario (?, ?, ?)';
-            
-                return true;
-            
+            await db.execute(sql, [nome, email, senha]);
+            return true;
         } catch (error) {
             console.error('Erro ao cadastrar usuário', error);  
-            return res.status(500).json({ error: "Erro ao cadastrar usuário" });
             throw error;  
         }
     },
-    
-
     verificarUsuario: async (email) => {
         //Método que verifica o email informado através da 'procedure'
         try {
             const [rows] = await db.execute('CALL buscar_usuario_por_email(?)', [email]);
             if (rows.length === 0) {
-                return res.status(404).json({error: 'Usuário não encontrado.'});
             }
             return rows[0];
             
         } catch (error) {
-            return res.status(500).json({ error: 'Erro interno ao buscar usuário.'});
+            throw new error
         }
     }
-
 };
 
 module.exports = usuarioModel;
