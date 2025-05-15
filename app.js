d17c1b2c15a97f99eb1175f0337098b423620ad8
aluno <etecae023@hotmail.com>
2025-05-15 19:59:59 -0300

    app.js Base - corrigido

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const usuarioController = require('./controllers/usuarioController');
+
+const app = express();
+const PORT = 3000;
+
+const path = require('path');
+
+app.use(express.json());
+
+
+//app.get('/api/usuario', usuarioController.getAllUser);
+//app.post('/api/usuario', usuarioController.createNewUser);
+
+app.listen(PORT, () => {
+ console.log(`Servidor na porta http://localhost:${PORT}`);
+});
