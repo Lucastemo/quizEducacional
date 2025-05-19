0c7cd123d7796aec298344cc9ac8e5587e4d2fd6
LucasTM <lucasteixeiramoraes15@gmail.com>
2025-05-19 00:38:23 -0300

    Criar arquivo questaoController.js para os métodos das rotas e criar as rotas em app.js para as questões

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const questaoController = require('./controllers/questaoController');
 const alternativaController = require('./controllers/alternativaController');
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+
+// ROTAS PARA questaoController
+app.get('/api/questao/:id_disciplina', questaoController.consultarQuestao);
+app.post('/api/questao', questaoController.cadastrarQuestao);
+app.put('/api/questao', questaoController.editarQuestao);
+app.delete('/api/questao/:id', questaoController.excluirQuestao);
 
 // ROTAS PARA alternativaController
 app.get('/api/alternativa/:id_questao', alternativaController.consultarAlternativa);
