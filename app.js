c84ea73e4582286b72812c07e386c68f9e9ad723
aluno <etecae023@hotmail.com>
2025-05-26 20:20:06 -0300

    rank.html e algumas alteracoes

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const path = require ('path')
+
 const questaoController = require('./controllers/questaoController');
 const alternativaController = require('./controllers/alternativaController');
 
 const app = express();
+
 const PORT = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
 // ROTAS PARA questaoController
